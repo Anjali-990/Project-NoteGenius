8196c32e34d696231f84dd83058d39f48ccd04a9
DevanshiGupta15 <git config --global user.email>
2025-12-01 22:55:34 +0530

    qna

--- myapp/views.py
+++ myapp/views.py
@@ -11,3 +11,8 @@ def audio_upload(request):
     return render(request, "audioup.html")
 def video_upload(request):
     return render(request, "videoup.html")
+
+from django.shortcuts import render
+#(text_upload, audio_upload, video_upload, etc.)
+def qna_upload(request):
+    return render(request, "QnAup.html") 
--- notegenius/urls.py
+++ notegenius/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('savednotes/', views.savednotes, name='savednotes'), # saved notes
     path("audio-upload/", views.audio_upload, name="audio_upload"), #audio upload
     path("video-upload/", views.video_upload, name="video_upload"), #video upload
+    path("qna-upload/", views.qna_upload, name="qna_upload"), #QnA generator upload
 
 
 ]
